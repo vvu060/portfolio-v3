c253229fcf784715baadaf2956162c053df84019
vvu060 <vvu060@gmail.com>
2024-08-24 18:53:31 +0530

    add hero section & spotlight component

---
+++ components/Hero.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Spotlight } from './ui/Spotlight';
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+        <Spotlight
+          className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen'
+          fill='white'
+        />
+         <Spotlight
+          className='top-10 left-full h-[80] md:-left-32 md:-top-20 h-screen'
+          fill='white'
+        />
+         <Spotlight
+          className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen'
+          fill='white'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
